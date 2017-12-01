4378ff67c40ebdc6df3ec6cd208e0abaa465f61a
stsdc <s.dac@yandex.ru>
2017-12-01 19:01:15 +0100

    receives and writes to serial

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,10 @@
 #include <vector>
 // #include <SPIFlash.h>
 
+// disables or enables printing to serial
+#define print(var) Serial.print(var)
+// #define print(var)
+
 #define NODEID 1
 #define NETWORKID 100
 #define FREQUENCY RF69_868MHZ  // Match this with the version of your Moteino! (others: RF69_433MHZ, RF69_868MHZ)
@@ -18,7 +22,9 @@ RFM69 radio;
 bool promiscuousMode = false; // set to 'true' to sniff all packets on the same network
 byte ackCount = 0;
 
-byte data[255];
+byte data[61];
+vector<byte> received;
+
 void setup() {
   Serial.begin(SERIAL_BAUD);
   delay(10);
@@ -44,36 +50,36 @@ void ping() {
   if (radio.ACKRequested()) {
     byte theNodeID = radio.SENDERID;
     radio.sendACK();
-    Serial.print(" - ACK sent.");
+    print(" - ACK sent.");
 
     // When a node requests an ACK, respond to the ACK
     // and also send a packet requesting an ACK (every 3rd one only)
     // This way both TX/RX NODE functions are tested on 1 end at the GATEWAY
     if (ackCount++ % 3 == 0) {
-      Serial.print(" Pinging node ");
-      Serial.print(theNodeID);
-      Serial.print(" - ACK...");
+      print(" Pinging node ");
+      print(theNodeID);
+      print(" - ACK...");
       delay(3);                                             // need this when sending right after reception .. ?
       if (radio.sendWithRetry(theNodeID, "ACK TEST", 8, 0)) // 0 = only 1 attempt, no retries
-        Serial.print("ok!");
+        print("ok!");
       else
-        Serial.print("nothing");
+        print("nothing");
     }
   }
-  Serial.println();
+  print("\n");
 }
 
 void show_debug_data() {
-  Serial.print("[FROM:");
-  Serial.print(radio.SENDERID, DEC);
-  Serial.print("] ");
-  Serial.print(" [RX_RSSI:");
-  Serial.print(radio.readRSSI());
-  Serial.print("]");
+  print("[FROM:");
+  print(radio.SENDERID);
+  print("] ");
+  print(" [RX_RSSI:");
+  print(radio.readRSSI());
+  print("]");
   if (promiscuousMode) {
-    Serial.print("to [");
-    Serial.print(radio.TARGETID, DEC);
-    Serial.print("] ");
+    print("to [");
+    print(radio.TARGETID);
+    print("] ");
   }
 }
 
@@ -81,17 +87,14 @@ void loop() {
   // process any serial input
   if (radio.receiveDone()) {
     show_debug_data();
-    Serial.print(radio.DATALEN);
-    if (radio.DATALEN != sizeof(data)) {
-      Serial.print("Invalid payload received!\n");
-    } else {
-
-      for (int i = 0; i < radio.DATALEN; i++) { // loop through all the new bytes
-        data[i] = radio.DATA[i];
-        Serial.write(data[i]);
-      }
-      ping();
-      blink(LED, 3);
+     for (int i = 0; i < radio.DATALEN; i++) { // loop through all the new bytes
+      // data[i] = radio.DATA[i];
+      // Serial.println();
+      // print(" ");
+      // print(radio.DATA[i]);
+      Serial.write(radio.DATA[i]);
     }
+    ping();
+    blink(LED, 3);
   }
 }
